2dde57807fe22e95c5f22f755e809961aad4d45a
BKowalskiOff <82583944+BKowalskiOff@users.noreply.github.com>
2023-07-13 03:51:28 +0200

    Time: 65 ms (88.06%), Space: 16.3 MB (85.06%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -5,9 +5,17 @@ class Solution:
             return s
         
         strings = ['' for i in range(numRows)]
+        row = 0
+        up = False
         
-        for i, c in enumerate(s):
-            row = abs(abs((i%(2*numRows-2))-(numRows-1))-(numRows-1))
+        for c in s:
+            if row == 0 or (row == (numRows - 1)):
+                up = not up
             strings[row] += c
-                
+            
+            if up:
+                row += 1
+            else: 
+                row -= 1
+            
         return ''.join(strings)
