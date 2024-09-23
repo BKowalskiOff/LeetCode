3d1306d4fe8012c6a49a0e1c535982af1124ff46
BKowalskiOff <82583944+BKowalskiOff@users.noreply.github.com>
2024-09-23 03:58:13 +0200

    Time: 46 ms (32.38%), Space: 17.8 MB (7.69%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -2,10 +2,14 @@ class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         out = ["" for _ in range(n)]
         for i in range(1, n+1):
+            if i%15 == 0:
+                out[i-1] = "FizzBuzz"
+                continue
             if i%3 == 0:
                 out[i-1] = "Fizz"
+                continue
             if i%5 == 0:
-                out[i-1] += "Buzz"
-            if out[i-1] == "":
-                out[i-1] = str(i)
+                out[i-1] = "Buzz"
+                continue
+            out[i-1] = str(i)
         return out
